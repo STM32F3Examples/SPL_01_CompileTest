318c2a58b4d2128a7a952fd5b7ad0d59db3d1d9b
Manuel Limones <limonesu.me@gmail.com>
2015-04-19 03:22:26 -0500

    add push button detection in main.c

--- source/main.c
+++ source/main.c
@@ -3,7 +3,25 @@
 
 void delay_ms(int delay_time);
 
+void led_init(void);
+void button_init(void);
+
 int main(){
+	led_init();
+	button_init();
+	while(1){
+		if(GPIO_ReadInputDataBit(GPIOC,GPIO_Pin_13)){
+			GPIO_ResetBits(GPIOB,GPIO_Pin_13);
+		} else{
+			GPIO_WriteBit(GPIOB,GPIO_Pin_13,Bit_SET);
+			delay_ms(0xFFFFF);
+			GPIO_WriteBit(GPIOB,GPIO_Pin_13,Bit_RESET);
+			delay_ms(0xFFFFF);
+		}
+	}
+}
+
+void led_init(void){
 	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOB,ENABLE);
 	GPIO_InitTypeDef myGPIO;
 	GPIO_StructInit(&myGPIO);
@@ -13,12 +31,15 @@ int main(){
 	myGPIO.GPIO_PuPd=GPIO_PuPd_NOPULL;
 	myGPIO.GPIO_Speed=GPIO_Speed_10MHz;
 	GPIO_Init(GPIOB,&myGPIO);
-	while(1){
-		GPIO_WriteBit(GPIOB,GPIO_Pin_13,Bit_SET);
-		delay_ms(0xFFFFF);
-		GPIO_WriteBit(GPIOB,GPIO_Pin_13,Bit_RESET);
-		delay_ms(0xFFFFF);
-	}
+}
+
+void button_init(void){
+	RCC_AHBPeriphClockCmd(RCC_AHBENR_GPIOCEN ,ENABLE);
+	GPIO_InitTypeDef myGPIO;
+	GPIO_StructInit(&myGPIO);
+	myGPIO.GPIO_Mode=GPIO_Mode_IN;
+	myGPIO.GPIO_Pin=GPIO_Pin_13;
+	GPIO_Init(GPIOC,&myGPIO);
 }
 
 void delay_ms(int delay_time){
